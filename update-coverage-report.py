e738140a453fcba433e76498b711cb0e48d8e28b
Philip Pang <pkcphilip@gmail.com>
2011-06-17 00:23:11 +0800

    Cleaned the code and reduce the number from 1636 to 1444

--- update-coverage-report.py
+++ update-coverage-report.py
@@ -6,7 +6,7 @@ import urllib
 
 PORT = 8080
 APPNAME = '.'
-SOURCE_DIRECTORIES= 'tournament,tictactoe,livejsonapi'
+SOURCE_DIRECTORIES = 'tournament,tictactoe,livejsonapi'
 
 # To just launch the server without coverage
 #coverage_proc = subprocess.Popen(['/usr/local/bin/dev_appserver.py', '.',
@@ -14,28 +14,28 @@ SOURCE_DIRECTORIES= 'tournament,tictactoe,livejsonapi'
 #							      '--port=8080'])
 
 coverage_proc = subprocess.Popen(['coverage',
-								  'run',
-								  '--source='+SOURCE_DIRECTORIES,
-								  '/usr/local/bin/dev_appserver.py',
-								  APPNAME,
-								  '--clear_datastore',
-								  '--port='+str(PORT),
-								  '--use_sqlite'], 
+                                'run',
+                                '--source=' + SOURCE_DIRECTORIES,
+                                '/usr/local/bin/dev_appserver.py',
+                                APPNAME,
+                                '--clear_datastore',
+                                '--port=' + str(PORT),
+                                '--use_sqlite'],
 #								  stdout=subprocess.PIPE,  #Comment out these lines to see stdout in console
 #								  stderr=subprocess.STDOUT #Comment out these lines to see stdout in console
-								  )
-								  
+                                  )
+
 print 'Waiting for server to launch'
-time.sleep(2) #Give the server some time to boot up before fetching the index
+time.sleep(2)  # Give the server some time to boot up before fetching the index
 
 data = ''
-try: 
+try:
   print 'Fetching index'
-  data = urllib.urlopen('http://localhost:8080').read() 
+  data = urllib.urlopen('http://localhost:8080').read()
   #print data
-  
+
   print 'Fetching test data'
-  data = urllib.urlopen('http://localhost:8080/test?format=plain').read() 
+  data = urllib.urlopen('http://localhost:8080/test?format=plain').read()
   print data
 
 except ValueError:
@@ -43,12 +43,12 @@ except ValueError:
   print ValueError
 
 if 'FAILED' in data:
-    print '*** Tests Failed ********************' 
+    print '*** Tests Failed ********************'
     print '*** Not calculating coverage ********'
 
-else:								  
+else:
   print '*** Creating html coverage report **'
-  coverage_proc2 = subprocess.Popen(['coverage', 'html']) 
+  coverage_proc2 = subprocess.Popen(['coverage', 'html'])
 
 print '*** Killing dev_appserver process ***'
 os.kill(coverage_proc.pid, signal.SIGINT)
